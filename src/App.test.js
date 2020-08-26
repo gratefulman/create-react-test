011124e4fc13db90354f00fd6d2fbc2048c46317
Artem Stupakov <artem@Mac-mini.local>
2020-08-26 07:16:36 +0300

    added abs

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+
+
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test('test math', ()=>{
+
+  const sum = Math.abs(-1);
+  expect(sum).toEqual(1);
+
 });
